1edd855fcc9c2d68d06dedc9758608372e8a5681
vishal-menon <vishaljmenon@gmail.com>
2024-06-22 16:06:39 +0530

    fix/Responsive Design - Homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     }, 2700);
 
     return (
-        <main className={`overflow-hidden flex flex-col justify-center h-screen w-screen bg-cloud bg-cover bg-center animate-backgroundShift`}>
+        <main className={`fixed overflow-hidden flex flex-col justify-center h-screen w-screen bg-cloud bg-cover bg-center animate-backgroundShift`}>
             {playVid ?
                 <div className="absolute inset-0 flex items-center justify-center">
                     <div className="relative w-full h-full">
@@ -31,31 +31,35 @@ export default function Home() {
             }
             <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 z-5"></div>
 
-            <div className='z-10 h-full w-screen flex flex-1 justify-center items-center'>
-                <p className="text-white text-[16px] font-kelly absolute top-6 left-1/2 transform -translate-x-1/2 font-bold">08/05/2024&nbsp;&nbsp;&nbsp;&nbsp;22:00 EST</p>
+            <div className='z-10  h-full w-screen flex flex-col flex-1 justify-center items-center'>
+                {/**<p className="text-white text-[16px] font-kelly absolute top-6 left-1/2 transform -translate-x-1/2 font-bold">08/05/2024&nbsp;&nbsp;&nbsp;&nbsp;22:00 EST</p>**/}
                 <Tilt className='parallax-effect h-full w-full shadow-2xl' perspective={1500}>
                     <div className='inner-element flex-col  justify-center grid place-content-center h-full w-full  my-5'>
                         <div className='flex flex-row'>
                             <div className='p-12 grid place-content-center'>
-                                <button className='text-7xl px-96 py-10 text-white italic font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>ABOUT</button>
+                                <button className='text-6xl px-96 text-white italic font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>about</button>
                             </div>
                         </div>
-                        <div className='flex justify-between gap-20'>
-                            <button className='h-full text-7xl text-white italic  text-center font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>MUSIC</button>
-                            <div className='rounded-2xl px-6 py-3'>
-                                <h1 className="text-9xl text-white text-center font-univers italic font-bold tracking-wide hover:drop-shadow-glow">sfl</h1>
+                        <div className='w-screen flex justify-between lg:flex-nowrap flex-wrap'>
+                            <div className='flex-1 flex md:justify-end justify-center md:min-w-0 min-w-full'>
+                            <button className='text-6xl text-white italic  text-center font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>music</button>
                             </div>
-                            <button className='text-7xl text-white italic text-center font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>VIDEOS</button>
+                            <div className='flex items-center justify-center items-center md:min-w-0 min-w-full md:mx-40 mx-0 md:my-10 my-20'>
+                            <h1 className="text-5xl text-white text-center font-univers italic font-bold tracking-wide hover:drop-shadow-glow">sfl</h1>
+                            </div>
+                            <div className='flex-1 flex md:justify-start justify-center md:min-w-0 min-w-full'>
+                            <button className='text-6xl text-white italic text-center font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>videos</button>
+                            </div>
                         </div>
                         <div className='flex flex-row'>
                             <div className='p-12 grid place-content-center'>
-                                <button className='text-7xl px-96 py-10 text-white italic font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>EVENTS</button>
+                                <button className='text-6xl px-96 text-white italic font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>events</button>
                             </div>
                         </div>
                     </div>
                 </Tilt>
             </div>
-            <Footer></Footer>
+            {/**<Footer></Footer>**/}
         </main>
     )
 }
