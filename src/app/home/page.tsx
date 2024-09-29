be58b452bdbe01c46ea2e435bfc8fd6cf32531f5
Krishnadas Nair <krishnadas.nair02@gmail.com>
2024-09-29 22:07:40 +0400

    fix: fixed offset on homePage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,30 +15,29 @@ export default function Home() {
                 <button className='font-bold font-univers drop-shadow-xl text-[18px]  hover:text-[#937666] transition-all'>SHOP</button>
             </nav>
 
-            <div className='fixed top-0 left-4 w-full h-full flex flex-col flex-1 justify-center items-center'>
 
-                <Tilt tiltMaxAngleY={20} tiltMaxAngleX={20} perspective={1500} transitionEasing='cubic-bezier(.5,.98,.52,.99)'>
-                    <div className='w-3/4 mx-auto flex-col justify-center grid place-content-center'>
-                        <div className='flex justify-center flex-row'>
-                            <button className='text-[48px] w-fit text-white italic font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>ABOUT</button>
+
+            <Tilt tiltMaxAngleY={20} tiltMaxAngleX={20} perspective={1500} transitionEasing='cubic-bezier(.5,.98,.52,.99)'>
+                <div>
+                    <div className='flex justify-center flex-row'>
+                        <button className='text-[48px] w-fit text-white italic font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>ABOUT</button>
+                    </div>
+                    <div className='w-full flex justify-between lg:flex-nowrap flex-wrap'>
+                        <div className='flex-1 flex md:justify-end justify-center md:min-w-0 min-w-full'>
+                            <button className='text-[48px] text-white italic  text-center font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>MUSIC</button>
                         </div>
-                        <div className='w-screen flex justify-between lg:flex-nowrap flex-wrap'>
-                            <div className='flex-1 flex md:justify-end justify-center md:min-w-0 min-w-full'>
-                                <button className='text-[48px] text-white italic  text-center font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>MUSIC</button>
-                            </div>
-                            <div className='flex-1 flex justify-center mx-20 my-20 items-center'>
-                                <h1 className="text-[36px] text-white font-univers pr-5 italic hover:drop-shadow-glow bg-black p-3 rounded-lg">CONCEPTS</h1>
-                            </div>
-                            <div className='flex-1 flex md:justify-start justify-center md:min-w-0 min-w-full'>
-                                <button className='text-[48px] text-white italic text-center font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>VIDEOS</button>
-                            </div>
+                        <div className='flex-1 flex justify-center mx-20 my-20 items-center'>
+                            <h1 className="text-[36px] text-white font-univers pr-5 italic hover:drop-shadow-glow bg-black p-3 rounded-lg">CONCEPTS</h1>
                         </div>
-                        <div className='flex flex-row justify-center'>
-                            <button className='text-[48px] text-white italic font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>EVENTS</button>
+                        <div className='flex-1 flex md:justify-start justify-center md:min-w-0 min-w-full'>
+                            <button className='text-[48px] text-white italic text-center font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>VIDEOS</button>
                         </div>
                     </div>
-                </Tilt>
-            </div>
-        </main>
+                    <div className='flex flex-row justify-center'>
+                        <button className='text-[48px] text-white italic font-bold font-univers drop-shadow-3xl hover:drop-shadow-glow'>EVENTS</button>
+                    </div>
+                </div>
+            </Tilt>
+        </main >
     )
 }
