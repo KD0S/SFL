e1b2b58a4f4e9ec75bb727fcf9ec2b9ac11fd0b9
Krishnadas Nair <krishnadas.nair02@gmail.com>
2024-10-04 12:03:51 +0400

    merge: merged changes for music videos

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -6,44 +6,44 @@ import ServerComponent from "../pages/api/musicVideo"
 export const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
-    const [playlist, setPlaylist] = useState("Hello World!");
-    const [embedId, setEmbedId] = useState("z6fnyWLLz1Q");
-    const [refresh, setRefresh] = useState(false);
+  const [playlist, setPlaylist] = useState("Hello World!");
+  const [embedId, setEmbedId] = useState("z6fnyWLLz1Q");
+  const [refresh, setRefresh] = useState(false);
 
-    const axiosInstance = axios.create({
-      baseURL: 'https://www.googleapis.com/youtube/v3',
-      timeout: 5000, // Timeout if necessary
-      header: {
-        'ContentType': 'program/json',
-        // Add all custom headers here
-      },
-    });
- 
-    
-    useEffect(() => {
+  const axiosInstance = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    timeout: 5000, // Timeout if necessary
+    header: {
+      'ContentType': 'program/json',
+      // Add all custom headers here
+    },
+  });
 
-      // Fetch data from backend
-      const fetchData = async () => {
 
-        const response = await (await fetch('/api/musicVideo')).json();
-        console.log(response)
-        setPlaylist(response);
-           
-      };
-  
-      fetchData();
-    }, [refresh]);
-  
-    const forceRefresh = useCallback(() => {
-      setRefresh(prev => !prev); // Toggle refresh state to trigger useEffect
-      }, []);
-  
-    return (
-      <DataContext.Provider value={{ embedId, setEmbedId, playlist, forceRefresh }}>
-        {children}
-      </DataContext.Provider>
-    );
-  };
-  
+  useEffect(() => {
+
+    // Fetch data from backend
+    const fetchData = async () => {
+
+      const response = await (await fetch('/api/musicVideo')).json();
+      console.log(response)
+      setPlaylist(response);
+
+    };
+
+    fetchData();
+  }, [refresh]);
+
+  const forceRefresh = useCallback(() => {
+    setRefresh(prev => !prev); // Toggle refresh state to trigger useEffect
+  }, []);
+
+  return (
+    <DataContext.Provider value={{ embedId, setEmbedId, playlist, forceRefresh }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
 
 export default DataProvider
