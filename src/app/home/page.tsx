7d9acbc452997334abd49708dc9a5cf2c49281ab
vishal-menon <vishaljmenon@gmail.com>
2025-05-01 20:08:30 +0530

    test

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -42,6 +42,8 @@ export default function Home() {
                     </div>
                 </div>
             </Tilt>
+
+            // test
             {/* <Footer /> */}
         </main >
     )
